a842ed539c1f491f23fbeddcc8664cf8ae71a447
NoBigHomies <Jalenawakes@gmail.com>
2025-02-24 02:22:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 # Basic Config (For all bots)
 pythonCommand = "python" # If you have the python command set to something else such as python3
-userToken = [''] # User Tokens here. example 'token','token' | 'email:pass', 'token'
-DiscordChannel = '' # Channel ID,
-SpamSpeed = 0.7 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
+userToken = ['MTMzOTg4MTQyNDM4NzMwOTY4Mg.G_pmSr.o3lXCTeatdmcLaK6IfRnU_3g9QwD3UWQ7yi8YY'] # User Tokens here. exa
+DiscordChannel = '1243491232580108310' # Channel ID,
+SpamSpeed = 0.7 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━
 textRandom = False # if the root has 'text.txt' it will pick a random text line and send it.
 textFull = False # Will send everything written in 'text.txt'
 discumLog = False # Displays Discums Logs, Very useful for providing more info on what went wrong.
 
 #Server Joiner
 useTokenJoin = True
-inviteLink = '' # Only the last 6 digits of the invite link https://discord.gg/XXXXXX | Unless you are using email join then just the full URL
+inviteLink = '' # Only the last 6 digits of the invite link https://discord.gg/XXXXXX | Unless you are using ema
 autojoinServer = False # Will auto join the inviteLink server if True
 useBrowser = 'Chrome' # Chrome | Firefox | Edge (windows only) | Set the browser used for joining the servers.
 joinSpeed = 3 # Changes how fast the bot will start the server joiner
 
 #DM Spam
-DiscordServer = '' # Server ID (https://i.imgur.com/m06TSEx.png this one)
-ScanAllServers = False # (True or False) Instead of using a server ID this will scan all of the users joined servers. 
+DiscordServer = '1243491228293664828' # Server ID (https://i.imgur.com/m06TSEx.png this one)
+ScanAllServers = False # (True or False) Instead of using a server ID this will scan all of the users joined ser
 HeavyScrape = False # (True or False) Use this for servers with a lot of users
 
 #Account Creator
@@ -24,4 +24,4 @@ captchaAPI = '' #2captcha API Key
 mailServer = '' # Mail server, pop 3 version. (example = "pop.mail.ru")
 
 #Picture spam
-DirPictures = 'C:\\Users\\Merubokkusu\\Pictures\\spam\\' # Picture(s) location, end directory with a double slash.
+DirPictures = 'C:\Users\jalen\Pictures\Discord Photos' # Picture(s) location, end directory with a double slash.
